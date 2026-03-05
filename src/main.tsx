228f21875fecb6d6d917c23857ad7ce56e81a7f7
Victory <nwanoruovictory87@gmail.com>
2026-03-05 03:29:35 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from 'react-router-dom';
 import "./index.css";
 import App from "./App.tsx";
 //*=============== context provider
@@ -7,8 +8,10 @@ import { UserDataProvider } from "./components/AppContext/AppContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <HashRouter>
     <UserDataProvider>
       <App />
     </UserDataProvider>
+      </HashRouter>
   </StrictMode>,
 );
